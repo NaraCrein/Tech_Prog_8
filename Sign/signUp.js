27accda3b12feed12f65ab7e998d458a5189876c
bifk <75476142+bifk@users.noreply.github.com>
2022-04-26 20:49:48 +0700

    Delete Sign directory

--- Sign/signUp.js
+++
@@ -1,6 +0,0 @@
-function check()
-{
-    if (document.querySelector('#first').value !== document.querySelector('#second').value ) {
-        document.querySelector('#first').value = "Пароли не совпадают"
-    }
-}
