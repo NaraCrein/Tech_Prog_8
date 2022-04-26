05a15fa843d6fc6f16318b91909ff7fb40cbaa4e
bifk <75476142+bifk@users.noreply.github.com>
2022-04-26 20:26:59 +0700

    Add files via upload

---
+++ Fixed/Sign/signUp.js
@@ -0,0 +1,13 @@
+function check()
+{
+    if (document.querySelector('#first').value !== document.querySelector('#second').value ) {
+        document.querySelector('.wrong').style.display = "block"
+        document.querySelector('.block').style.height = "calc(580px + 20px)"
+        return
+    }
+    else
+    {
+        document.querySelector('.wrong').style.display = "none"
+        document.querySelector('.block').style.height = "580px"
+    }
+}
