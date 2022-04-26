86b07fac3750232a1f2693e98398471f40d4f71b
bifk <75476142+bifk@users.noreply.github.com>
2022-04-26 21:03:00 +0700

    Add files via upload

--- Profile/profile.js
+++ Profile/profile.js
@@ -2,3 +2,19 @@
 {
     window.location.href = '../Main/main.html';
 }
+
+function change()
+{
+    if (document.querySelector('#change').value === "Редактировать")
+    {
+        document.querySelector('#change').value = "Сохранить"
+        document.querySelector('.inp').disabled = ""
+        document.querySelector('#inp1').disabled = ""
+    }
+    else if (document.querySelector('#change').value === "Сохранить")
+    {
+        document.querySelector('#change').value = "Редактировать"
+        document.querySelector('.inp').disabled = "disabled"
+        document.querySelector('#inp1').disabled = "disabled"
+    }
+}
