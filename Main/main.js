cde049e3fc9ba54267747415cce4c3d5aa519736
NaraCrein <93971757+NaraCrein@users.noreply.github.com>
2022-05-11 00:06:26 +0700

    Delete Main directory

--- Main/main.js
+++
@@ -1,9 +0,0 @@
-function goToSign(flag) {
-    if (flag){
-        window.location.href = '../Profile/Profile.html';
-    }
-    window.location.href = '../Sign/SignIn.html';
-}
-function goToDetail(){
-    window.location.href = '../Detail/detail.html';
-}
